d459c559393cc6e77c04912fc109c29c1e5c8575
Altendorfner <BA_SPM@e.mail.de>
2026-04-15 19:11:26 +0200

    FIX - win path handling - fixed linux code passage for windows usage

--- src/train_random_forest/feature_engineering.py
+++ src/train_random_forest/feature_engineering.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def delta_date_feature(dates):
     """
-    Given a 2d array containing dates (in any format recognized by pd.to_datetime), 
+    Given a 2d array containing dates (in any format recognized by pd.to_datetime),
     it returns the delta in days between each date and the most recent date in its column
     """
     date_sanitized = pd.DataFrame(dates).apply(pd.to_datetime)
